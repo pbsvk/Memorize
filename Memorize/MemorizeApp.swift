45cf2364a13b6a5c37b1b003f4a4379d859319e2
pbsvk <152455231+pbsvk@users.noreply.github.com>
2024-08-21 11:58:33 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Bhaskara Sai Vamsi Krishna Padala on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
